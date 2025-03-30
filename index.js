747b0d3bf29b646e1d7d9ceac1d5058e97ac7809
kasalava <kasalava86@gmail.com>
2025-03-30 23:31:55 +0500

    function

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// const country = "Russia";
-// console.log(`я живу в ${country}`);
-// const x = null;
-// const num1 = 54;
-// const num2 = 55;
-// console.log(num1 == num2);
-// console.log(num1 === num2);
-const age = 17
-if (age<18)
-        {console.log("продажа алкоголя запрещена")}
+
+function greet(name){
+
+    console.log(`привет ${name}`)
+}
+greet('anton')
