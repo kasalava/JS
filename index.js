9bf20bdcb7a341e9f68d477e8a73d0333ec6ab55
kasalava <kasalava86@gmail.com>
2025-03-30 23:53:14 +0500

    funk add

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
-function greet(name){
-
-    console.log(`привет ${name}`)
+// function greet(name) {
+//   if (name === undefined) console.log("Привет!");
+//   else console.log(`привет ${name}`);
+// }
+// greet("");
+function add(num1, num2) {
+  const res = num1 + num2;
+  console.log(`result:`, res);
 }
-greet('anton')
+add(1, 5);
