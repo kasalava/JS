39f4e9a301d4f01b507d12d34c5c0b5e475f3023
kasalava <kasalava86@gmail.com>
2025-03-31 23:16:34 +0500

    Celcium to Farengeit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const milesToKilometers = (miles) => miles * 1.609344;
-console.log(milesToKilometers(1));
+// const milesToKilometers = (miles) => miles * 1.609344;
+// console.log(milesToKilometers(1));
 
-
+const cToF = (c) => c * 1.8 + 32;
+console.log(cToF(12));
