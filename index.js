a9c05e2208c57c5dcce87c472dcc482db4420144
kasalava <kasalava86@gmail.com>
2025-04-01 00:00:46 +0500

    ternar operator

--- index.js
+++ index.js
@@ -4,14 +4,8 @@
 // const cToF = (c) => c * 1.8 + 32;
 // console.log(cToF(12));
 
-const switchTemp = (deg, scale) => {
-  if (scale === "c") {
-    return deg * 1.8 + 32;
-  } else if (scale === "f") {
-    return (deg - 32) / 1.8;
-  } else {
-    return NaN;
-  }
-};
+const switchTemp = (deg, scale) =>
+  scale === "c" ? deg * 1.8 + 32 : (deg - 32) / 1.8;
+
 console.log(switchTemp(-40, "c"));
-console.log(switchTemp(0, "f"));
+console.log(switchTemp(-40, "f"));
