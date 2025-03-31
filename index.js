7eaed97582dc6e254887c5d38ea222daa4e6b607
kasalava <kasalava86@gmail.com>
2025-03-31 22:46:25 +0500

    miles to km

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-// function greet(name) {
-//   if (name === undefined) console.log("Привет!");
-//   else console.log(`привет ${name}`);
-// }
-// greet("");
-const toCapital = function (str) {
-  const firstChar = str[0];
-  const leftover = str.slice(1);
-  const capitalFirstLetter = firstChar.toUpperCase();
-  const lowerLeftover = leftover.toLowerCase();
-  const result = capitalFirstLetter + lowerLeftover;
-  return result;
-};
-console.log(toCapital("саша"));
+const milesToKilometers = (miles)=>{
+    const result = miles*1.609344
+    return result
+}
+console.log(milesToKilometers(1))
