c9b3f7e2cd0bf31ae6af708564a8f1db8ab1a290
kasalava <kasalava86@gmail.com>
2025-04-03 00:22:47 +0500

    countSpaces while

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 function countSpaces(text) {
-  //ф-я принимает аргумент(текст)
-  let counter = 0; //счетчик с 0
-  for (let i = 0; i < text.length; i++) {
-    /*кол-во пробелов не должно быть больше длины текста
-                                     счетчик увеличивается на 1 */
-    const letter = text[i]; //получаем букву из текста
-    if(letter === ' ') counter++; //если буква пробел, то счетчик увеличиваем на 1
+  let counter = 0;
+  let index = 0; //счетчик с 0
+  while (index < text.length) {
+    const letter = text[index]; //обпащаемся к тексту
+    if (letter === " ") counter++;
+    index++; //счетчик юудет увеличиваться на 1
   }
   return counter;
 }
-console.log(countSpaces('z fjfj  kfkk fjfj  kjf  f     ')); //вызываем ф-ю
+console.log(countSpaces('z fjfjkfkk fjfj  kjf  f  '));
