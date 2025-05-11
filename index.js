f75224c9aa1adb1b6d7e815ab1d8377e82c65bc1
kasalava <kasalava86@gmail.com>
2025-05-11 23:09:09 +0500

    alcohol age

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const num1 = '81'
-const num2 = `2`
-console.log(num1*num2)
+// const num1 = '81'
+// const num2 = `2`
+// console.log(num1*num2)
+const age = 18
+if(age>=18){
+    console.log('продажа алкоголя разрешена')}
+    else {
+    console.log('продажа алкоголя не разрешена')
+}
 
 
 
