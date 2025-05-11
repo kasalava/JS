6ab7b380d5f71dc2a05bb0a4a07464c68f1f8557
kasalava <kasalava86@gmail.com>
2025-05-11 23:16:57 +0500

    temp

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // const num1 = '81'
 // const num2 = `2`
 // console.log(num1*num2)
-const age = 18
-if(age>=18){
-    console.log('продажа алкоголя разрешена')}
-    else {
-    console.log('продажа алкоголя не разрешена')
-}
+// const age = 18
+// if(age>=18){
+//     console.log('продажа алкоголя разрешена')}
+//     else {
+//     console.log('продажа алкоголя не разрешена')
+// }
 
+let temp = 7;
+if (temp > 20) {
+  console.log("тепло");
 
-
-
-
-
+} else if (temp > 10) {
+  console.log("свежо");
+} else if (temp > 5) {
+    console.log("прохладно");}
+else {console.log("холодно");}
