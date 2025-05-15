a09e219e1f1dbaa0e2c8625e443d83b3b0d51cce
kasalava <kasalava86@gmail.com>
2025-05-15 23:41:13 +0500

    try catch

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-function add (num1 = 0, num2 = 0){
-const res = num1+num2
-return (res)
+
+const toCapital = function (str) {
+    try{//блок трай кэч - запускает блок кода
+  const firstChar = str[0]; 
+  const leftover = str.slice(1); 
+  const capitalFirstLetter = firstChar.toUpperCase(); 
+  const lowerLeftover = leftover.toLowerCase();
+  const result = capitalFirstLetter + lowerLeftover; 
+  return result; 
+    }catch(err){ 
+        console.log('произошла ошибка')//делаем с ошибкой чтото, например выводим сообщение об ошибке
+        return('') //всегда оборачиваем ф-ю в тарй кэч
+
+    }
 }
-const num = add (1,11 )
-console.log (`результат ${num}`)
+
+console.log(toCapital('саша')) 
+console.log(toCapital('ЛЕША'))
+console.log(toCapital('иГорЬ'))
+console.log(toCapital(''))
+console.log(toCapital('риА'))
+
+
+ 
