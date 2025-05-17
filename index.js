f374301f1ee501d97a0594ccf0366976f38a06bb
kasalava <kasalava86@gmail.com>
2025-05-18 01:16:26 +0500

    chaining

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-const switchTempature = (deg, scale)=>scale === 'C'? deg*1.8+32 : (deg-32)/1.8//если условие верно, то тернарный оператор выполнит условие, а если не верно, то выполнит код после двоеточия
-
-console.log(switchTempature(0, 'C'))
-console.log(switchTempature(0, 'F'))
+const str = 'i love js'.slice(3).replace('s', 'S')
+console.log(str)
